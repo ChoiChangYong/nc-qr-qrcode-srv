71ebab24ed851bebcb234c1f31ed3191887e92cf
ChoiChangYong <34606508+ChoiChangYong@users.noreply.github.com>
2019-08-02 19:49:06 +0900

    promise, async, await를 사용하여 callback 구조 리팩토링

--- controllers/qrlogin.js
+++ controllers/qrlogin.js
@@ -4,20 +4,19 @@ var Session = require('../services').Session;
 /**
  * QR코드 생성
  */
-exports.createQrcode = (req, res, next) => {
+exports.createQrcode = async (req, res, next) => {
     const request = {
         instanceId: req.params.instanceId
     }
 
-    Qrcode.createQRString((url) => {
-        // QR코드 세션 생성
-        req.session.instanceId = request.instanceId;
-        console.log(req.session);
-        req.session.save(() => {
-            res.json({
-                result: 1,
-                qrcode: url+'%3Fkey%3D'+req.sessionID+'&apn=com.example.qrcodelogin'
-            });
+    const createQRStringResult = await Qrcode.createQRString();
+    // QR코드 세션 생성
+    req.session.instanceId = request.instanceId;
+    console.log(req.session);
+    req.session.save(() => {
+        res.json({
+            result: 1,
+            qrcode: createQRStringResult+'%3Fkey%3D'+req.sessionID+'&apn=com.example.qrcodelogin'
         });
     });
 };
@@ -25,40 +24,41 @@ exports.createQrcode = (req, res, next) => {
 /**
  * QR코드 토큰 검증
  */
-exports.verifyQrcodeSession = (req, res, next) => {
+exports.verifyQrcodeSession = async (req, res, next) => {
     const request = {
         qrcodeSessionID: req.body.qrcodeSessionID
     }
-
-    if(request.qrcodeSessionID){
-        Session.verifyQrcodeSession(request.qrcodeSessionID, (callback) => {
-            if(callback!=null){
-                res.json({result: 1, qrcodeSession: callback});
-            } else {
-                res.json({result: 0, message: "세션이 만료되었습니다.\n다시 로그인해주세요!"});
-            }
-        });
-    } else {
-        res.json({result: 0, message: "로그인 정보가 존재하지 않습니다."});
+    
+    if(!request.qrcodeSessionID){
+        return res.json({result: 0, message: "로그인 정보가 존재하지 않습니다."});
     }
+
+    const verifyQrcodeSessionResult = await Session.verifyQrcodeSession(request.qrcodeSessionID);
+    
+    if(verifyQrcodeSessionResult==null){
+        return res.json({result: 0, message: "세션이 만료되었습니다.\n다시 로그인해주세요!"});
+    }
+    
+    res.json({result: 1, qrcodeSession: verifyQrcodeSessionResult});
 };
 
 /**
  * 유저 세션 삭제
  */
-exports.deleteQrcodeSession = (req, res, next) => {
+exports.deleteQrcodeSession = async (req, res, next) => {
     const request = {
         sessionID: req.params.sessionID
     }
 
-    if(request.sessionID){
-        Session.deleteSession(request.sessionID, (callback) => {
-            if(callback==1)
-                res.json({result: 1, message: "세션 삭제 성공"});
-            else
-                res.json({result: 0, message: "세션 삭제 실패! 삭제된 row 개수:"+callback});
-        });
-    } else {
-        res.json({result: 0, message: "유저 세션이 존재하지 않습니다."});
+    if(!request.sessionID){
+        return res.json({result: 0, message: "유저 세션이 존재하지 않습니다."});
+    } 
+
+    const deleteSessionResult = await Session.deleteSession(request.sessionID);
+    
+    if(deleteSessionResult!=1){
+        return res.json({result: 0, message: "세션 삭제 실패! 삭제된 row 개수:"+deleteSessionResult});
     }
+    
+    res.json({result: 1, message: "세션 삭제 성공"});
 };
--- services/session.js
+++ services/session.js
@@ -4,26 +4,30 @@ var Session = models.Session;
 /**
  * QR코드 세션 검증
  */
-exports.verifyQrcodeSession = (qrcodeSessionID, callback) => {
-    console.log('qrcodeSessionID : '+qrcodeSessionID);
-    Session.findOne({
-        where: { session_id: qrcodeSessionID }
-    }).then(function(result) {
-        callback(result);
-    }).catch(function(err){
-        console.log(err);
+exports.verifyQrcodeSession = (qrcodeSessionID) => {
+    return new Promise(resolve => { 
+        console.log('qrcodeSessionID : '+qrcodeSessionID);
+        Session.findOne({
+            where: { session_id: qrcodeSessionID }
+        }).then(function(result) {
+            resolve(result);
+        }).catch(function(err){
+            console.log(err);
+        });
     });
 }
 
 /**
  * 세션 삭제
  */
-exports.deleteSession = (sessionID, callback) => {
-    Session.destroy({
-        where: { session_id: sessionID }
-    }).then(function(result) {
-        callback(result);
-    }).catch(function(err){
-        console.log(err);
+exports.deleteSession = (sessionID) => {
+    return new Promise(resolve => { 
+        Session.destroy({
+            where: { session_id: sessionID }
+        }).then(function(result) {
+            resolve(result);
+        }).catch(function(err){
+            console.log(err);
+        });
     });
 };
